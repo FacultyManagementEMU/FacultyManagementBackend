c31f478dfe2344e9ff9fb740fd1cd14cb430940f
Jahoth <37456795+Jahoth@users.noreply.github.com>
2018-10-25 18:19:16 -0400

    Add files via upload

---
+++ api/v1/research.php
@@ -0,0 +1,23 @@
+<?php
+include("../functions/functions.php");
+include("../access.php");
+
+$request_method=$_SERVER["REQUEST_METHOD"];
+switch($request_method)
+	{
+		case 'GET':
+			// Retrive Products
+			if(!empty($_GET["id"])){
+				$id=intval($_GET["id"]);
+				getResearch($id);
+			}
+			else{
+				getResearch();
+			}
+			break;
+		default:
+			// Invalid Request Method
+			header("HTTP/1.0 405 Method Not Allowed");
+			break;
+	}
+?>
